37a0f1672c0ea723dba4894ee2dbdbb8e1a97d03
Javier Marquez <jamarquez145@gmail.com>
2021-10-22 05:06:16 -0300

    Commit rama desarrollo

--- 01.js
+++ 01.js
@@ -1,8 +1,22 @@
 //Instancia de Vue
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    titulo: '¡Hola mundo, con Vue!',
-    frutas: ['Manzana', 'Pera', 'Naranja']
-  }
-})
+    titulo: "¡Hola mundo, con Vue!",
+    frutas: ["Manzana", "Pera", "Naranja"],
+    frutasObjeto: [
+      { nombre: "Manzanas", cantidad: 10 },
+      { nombre: "Peras", cantidad: 0 },
+      { nombre: "Naranjas", cantidad: 12 },
+    ],
+    nuevaFruta: "",
+  },
+  methods: {
+    agregarFruta() {
+      this.frutasObjeto.push({
+        nombre: this.nuevaFruta,
+        cantidad: 0,
+      });
+    },
+  },
+});
